9b1cf1aa2ede07e80e15c3a5b5a48a6e0623bea9
NolanPG <nolanperez35@gmail.com>
2024-05-27 19:16:57 +0200

    Removed redundant bluetooth declaration

--- hosts/laptop/network.nix
+++ hosts/laptop/network.nix
@@ -24,10 +24,6 @@
       allowedUDPPortRanges = [{ from = 1714; to = 1764; }];
     };
 
-  # Enabling Bluetooth
-  hardware.bluetooth.enable = true;
-
-
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
