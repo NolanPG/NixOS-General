d5c51417b27da2feebb185e46b3d2a2b9349ec9c
NolanPG <nolanperez35@gmail.com>
2024-06-14 03:39:39 +0200

    Added accelerometer hardware support

--- hosts/laptops/yoga/default.nix
+++ hosts/laptops/yoga/default.nix
@@ -16,6 +16,8 @@
       ./network.nix
     ];
 
+    hardware.sensor.iio.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older ` NixOS versions.
   #
