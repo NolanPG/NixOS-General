dd2716d985c8d1683d4b65c4ecb03e72590342f4
NolanPG <nolanperez35@gmail.com>
2024-06-15 11:23:47 +0200

    Added twingate

--- hosts/laptops/ideapad/default.nix
+++ hosts/laptops/ideapad/default.nix
@@ -16,6 +16,10 @@
       ./network.nix
     ];
 
+    environment.systemPackages = with pkgs; [
+      twingate
+    ];
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older ` NixOS versions.
   #
