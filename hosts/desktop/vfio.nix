33fb7a1f03c7c77a795bd02af77d0b570b59fec5
NolanPG <nolanperez35@gmail.com>
2024-06-24 00:19:35 +0200

    Removed vfio passthrough config, added vmware

--- hosts/desktop/vfio.nix
+++ hosts/desktop/vfio.nix
@@ -1,56 +1,35 @@
 { pkgs, lib, config, ... }:
 
+{ 
+  # VirtualBox
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
 
-
-let
-  # RX 6650 XT
-  gpuIDs = [
-    "1002:73ef" # Graphics
-    "1002:ab28" # Audio
+  users.extraGroups.vboxusers.members = [ "nolan" ];
+  
+  # Virt-Manager
+  environment.systemPackages = with pkgs; [
+    # Apps for VFIO
+    looking-glass-client
+    virglrenderer
   ];
-in {
-  options.vfio.enable = with lib;
-    mkEnableOption "Configure the machine for VFIO";
 
-  config = let cfg = config.vfio; in {
-    boot = {
-      initrd.kernelModules = [
-        "vfio_pci"
-        "vfio"
-        "vfio_iommu_type1"
-      ];
-
-      kernelParams = [
-        # enable IOMMU
-        "amd_iommu=on"
-      ] ++ lib.optional cfg.enable
-        # isolate the GPU
-        ("vfio-pci.ids=" + lib.concatStringsSep "," gpuIDs);
-    };
-
-    environment.systemPackages = with pkgs; [
-      # Apps for VFIO
-      looking-glass-client
-      virt-manager
-    ];
-
-    specialisation."VFIO".configuration = {
-      system.nixos.tags = [ "with-vfio" ];
-      vfio.enable = true;
-    };
-
-    virtualisation.libvirtd = {
-      enable = true;
-      qemu.ovmf.enable = true;
-      qemu.runAsRoot = false;
-      onBoot = "ignore";
-      onShutdown = "shutdown";
-    };
-
-    users.users.nolan.extraGroups = [ "libvirtd" ];
-
-    hardware.graphics.enable = true;
-    virtualisation.spiceUSBRedirection.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.ovmf.enable = true;
+    qemu.runAsRoot = false;
+    onBoot = "ignore";
+    onShutdown = "shutdown";
   };
-  # Added libvirtd and apps for the virtual machine in configuration.nix
+
+  programs.virt-manager.enable = true;
+
+  # VMWare
+  virtualisation.vmware.host.enable = true;
+
+  users.users.nolan.extraGroups = [ "libvirtd" ];
+
+  hardware.graphics.enable = true;
+
+  virtualisation.spiceUSBRedirection.enable = true;
 }
