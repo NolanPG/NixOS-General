fa7c8b9ddbed57720b2efeea37da650ab60a08ea
NolanPG <nolanperez35@gmail.com>
2024-07-01 13:48:39 +0200

    Defined ntilde and Ntilde for UK keyboard

---
+++ hosts/laptops/yoga/keyboard/keyboard.nix
@@ -0,0 +1,7 @@
+{pkgs}:
+let
+  compiledLayout = pkgs.runCommand "keyboard-layout" {} ''
+    ${pkgs.xorg.xkbcomp}/bin/xkbcomp ${./layout.xkb} $out
+  '';
+in
+  services.xserver.displayManager.sessionCommands = "${pkgs.xorg.xkbcomp}/bin/xkbcomp ${compiledLayout} $DISPLAY";
