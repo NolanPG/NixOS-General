72dbb34d895b0654403c01122c05d51a8ce50ea8
NolanPG <nolanperez35@gmail.com>
2024-07-14 11:15:18 +0200

    Removed ratbagd daemon

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -18,8 +18,6 @@
 
   boot.loader.grub.useOSProber = true;
 
-  services.ratbagd.enable = true;
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older ` NixOS versions.
   #
@@ -37,6 +35,5 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
 
