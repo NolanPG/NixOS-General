947d3e1fa0c70a7630a6e5ffef786c90b9d267be
NolanPG <nolanperez35@gmail.com>
2024-09-23 22:07:29 +0200

    Changed programs.kitty.theme to programs.kitty.themeFile

--- home/home.nix
+++ home/home.nix
@@ -69,7 +69,7 @@
     font.name = "MesloLGS NF";
     font.package = pkgs.meslo-lgs-nf;
     font.size = 11;
-    theme = "Tokyo Night";
+    themeFile = "Tokyo Night";
     extraConfig = "
       background_opacity .85
       confirm_os_window_close 0
